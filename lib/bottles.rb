99e0cd0041d0678080d396732eb21a14364516c6
Kelly Ma <kelly.ma@shopify.com>
2022-08-07 17:13:33 -0400

    without #successfor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,40 +15,25 @@ class Bottles
   end
 
   def verse(number_of_bottles)
-    case number_of_bottles
-    when 0
-      lyrics_for_0_bottles
-    when 1
-      lyrics_for_1_bottle
-    else
-      lyrics_for(number_of_bottles)
-    end
-  end
-
-  def lyrics_for_0_bottles
-    "No more bottles of beer on the wall, " +
-    "no more bottles of beer.\n" +
-    "Go to the store and buy some more, " +
-    "99 bottles of beer on the wall.\n"
-  end
-
-  def lyrics_for_1_bottle
-    "1 bottle of beer on the wall, " +
-    "1 bottle of beer.\n" +
-    "Take #{pronoun(1)} down and pass it around, " +
-    "#{quantity(0)} #{container(0)} of beer on the wall.\n"
-  end
-
-  def lyrics_for(number_of_bottles)
-    "#{number_of_bottles} bottles of beer on the wall, " +
-    "#{number_of_bottles} bottles of beer.\n" +
-    "Take #{pronoun(number_of_bottles)} down and pass it around, " +
+    "#{quantity(number_of_bottles).capitalize} #{container(number_of_bottles)} of beer on the wall, " +
+    "#{quantity(number_of_bottles)} #{container(number_of_bottles)} of beer.\n" +
+    action(number_of_bottles) +
     "#{quantity(number_of_bottles - 1)} #{container(number_of_bottles - 1)} of beer on the wall.\n"
   end
 
+  def action(number_of_bottles = :FIXME)
+    if number_of_bottles == 0
+      "Go to the store and buy some more, "
+    else
+      "Take #{pronoun(number_of_bottles)} down and pass it around, "
+    end
+  end
+
   def quantity(number_of_bottles)
+    number_of_bottles = number_of_bottles % 100
+
     if number_of_bottles > 0
-      number_of_bottles
+      number_of_bottles.to_s
     else
       "no more"
     end
