6960206d060bb1ce0a300233956e2b7ad6546f5b
Kelly Ma <kelly.ma@shopify.com>
2022-08-10 22:52:22 -0400

    Add BottleNumber0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -76,3 +76,17 @@ class BottleNumber
     end
   end
 end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    "no more"
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+
+  def successor
+    99
+  end
+end
